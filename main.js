76aacb2f019a76cc295e493213acde259f5b68ff
Matushini97 <98423790+Matushini97@users.noreply.github.com>
2023-06-16 20:44:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ document.querySelector('#app').innerHTML = `
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="counter" type="button">123</button>
     </div>
     <p class="read-the-docs">
       Click on the Vit
